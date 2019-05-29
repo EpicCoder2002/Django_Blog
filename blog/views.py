63b7d6a35b5024087b60a24cc068cbb80efc9f33
EpicCoder2002 <ahmed.coder.2002@gmail.com>
2019-05-29 06:23:43 +0300

    Working on Templating and get better idea of it

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Ahmed Hosam',
+        'title': 'First Title',
+        'content': 'First Content',
+        'date_posted': '2002-10-14'
+    },
+    {
+        'author': 'Mohammed Hosam',
+        'title': 'Second Title',
+        'content': 'Second Content',
+        'date_posted': '2009-12-05'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
