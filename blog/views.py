4f2db740dd438ef05b588141af7b8f32ef7c9487
EpicCoder2002 <ahmed.coder.2002@gmail.com>
2019-06-05 07:04:36 +0300

    Working on Reset Passwrd Reset System

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 5
+    paginate_by = 1
 
 class UserPostListView(ListView):
     model = Post
